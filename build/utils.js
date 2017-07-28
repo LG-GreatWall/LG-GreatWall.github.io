a1f9f30f01ac14899fd9b1029316027a3ecdec11
FE—李 长城 <changcheng.li@quantgroup.cn>
2017-07-28 14:39:07 +0800

    个人豆瓣观影网站搭建

--- build/utils.js
+++ build/utils.js
@@ -19,10 +19,17 @@ exports.cssLoaders = function (options) {
       sourceMap: options.sourceMap
     }
   }
+  // 自动将px安装rem
+  var px2remLoader = {
+      loader: 'px2rem-loader',
+      options: {
+        remUnit: 75
+      }
+    }
 
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    var loaders = [cssLoader]
+    var loaders = [cssLoader, px2remLoader]
     if (loader) {
       loaders.push({
         loader: loader + '-loader',
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -62,6 +62,8 @@ module.exports = {
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
+
     ]
-  }
+ },
+
 }
